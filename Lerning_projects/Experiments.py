7fb817b8b6d097c75e5b00b9482b27d1a16fd636
PavelVes <paxav@mail.ru>
2023-05-01 21:13:17 +0300

    Solution of problem 1.1 in three variants

--- Lerning_projects/Experiments.py
+++ Lerning_projects/Experiments.py
@@ -2,15 +2,24 @@
 
 s = 'Всем привет - товаристчи - коллеги - сотрудники!'
 
-s = 'Hello World'
+#s = 'Hello World'
 
 s1 = s.find('l')
 s2 = s.rfind('l')
 
-print(s1, s2)
 
 
-print(dir(s1), sep='\n ')
+print(s.find('-', 13))
+
+
+
+
+
+
+
+#print(s)
+
+
 
 
 
